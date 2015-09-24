9099d464de3c483afb2d102a1c1a066b205c98c5
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-24 13:18:20 +0700

    revert

--- js/ng_firebase_helper.js
+++ js/ng_firebase_helper.js
@@ -1,54 +1,90 @@
-window.FIREBASE_URL = "https://flickering-fire-25.firebaseio.com";
-
 angular.module('firebaseHelper', [])
-.service('firebaseHelper', function($firebaseObject, $firebaseArray, $firebaseObject, $firebaseAuth, $rootScope, $state, notify) {
+
+.provider('firebaseHelperConfig', [function() {
+    var endpoint = "";
+    this.setURL = function(url) {
+        endpoint = url;
+    }
+    this.$get = [function() {
+        return endpoint;
+    }]
+}])
+
+.service('firebaseHelper', function($firebaseObject, $firebaseArray, $firebaseObject, $firebaseAuth, $rootScope, $state, notify, firebaseHelperConfig) {
     var self = this;
 
     this.getFireBaseInstance = function(key) {
-        return new Firebase(key?FIREBASE_URL + "/" + key:FIREBASE_URL);
+        key = getPath(key);
+        return new Firebase(key?firebaseHelperConfig + "/" + key:firebaseHelperConfig);
     }
 
-    this.bindObject = function(path, $scope, key) {
-        console.log("bindObject", path);
-        var syncObject = $firebaseObject(self.getFireBaseInstance(path));
+    this.buildPath = function(arr) {
+        return arr.join("/")
+    }
+
+    var getPath = function(p) {
+        if (!p) {
+            return p;
+        }
+        if (typeof(p) == "string") {
+            return p;
+        }
+        return self.buildPath(p);
+    }
+    var getRef = function(p) {
+        if (typeof(p) == "object" && p.length == 0 || typeof(p.length) == "undefined") {
+            return p;
+        } else {
+            return self.getFireBaseInstance(getPath(p));
+        }
+    }
+
+
+    this.bindObject = function(ref, $scope, key) {
+        ref = getRef(ref);
+        var syncObject = $firebaseObject(ref);
         syncObject.$bindTo($scope, key);
     }
 
-    this.syncObject = function(path) {
-        console.log("syncObject", path);
-        return $firebaseObject(self.getFireBaseInstance(path));
+    this.syncObject = function(ref) {
+        ref = getRef(ref);
+        return $firebaseObject(ref);
     }
 
     this.syncProtectedObject = function(path) {
-        console.log("syncProtectedObject", path);
+        path = getPath(path);
         return $firebaseObject(self.getFireBaseInstance(path + "/" + self.getUID()));
     }
 
-    this.syncArray = function(path) {
-        console.log("syncArray", path);
-        return $firebaseArray(self.getFireBaseInstance(path));
+    this.syncArray = function(ref) {
+        ref = getRef(ref);
+        return $firebaseArray(ref);
     }
 
     this.syncProtectedArray = function(path) {
-        console.log("syncArray", path + "/" + self.getUID());
+        path = getPath(path);
         return $firebaseArray(self.getFireBaseInstance(path + "/" + self.getUID()));
+    }
+
+    this.transaction = function(ref, f) {
+        ref = getRef(ref);
+        ref.transaction(function(current_val) {
+            if (f) {return f(current_val);}
+            return current_val;
+        })
     }
 
     this.auth = $firebaseAuth(self.getFireBaseInstance());
     this.authData = null;
     this.profileData = null;
     this.auth.$onAuth(function(authData) {
-        console.log("$onAuth", authData);
+        // console.log("$onAuth", authData);
         self.authData = authData;
         if (authData) {
             self.syncObject("profiles/" + self.getUID()).$loaded(
                 function (data) {
                     self.profileData = data;
                     $rootScope.$broadcast('user:login',authData);
-                    // if (data.role !== "admin") {
-                    //     $state.go("login");
-                    //     $rootScope.notifyError("Invalid permission");
-                    // }
                 },
                 function (error) {
                     $rootScope.notifyError("Fail to get data");
@@ -82,6 +118,15 @@ angular.module('firebaseHelper', [])
         return "";
     }
 
+    this.getGravatar = function() {
+        if (this.authData) {
+            if (this.authData.password && this.authData.password.email) {
+                return "http://www.gravatar.com/avatar/" + md5(this.authData.password.email) + "?s=200&r=pg&d=mm";
+            }
+        }
+        return "http://www.gravatar.com/avatar/" + md5("nothing") + "?s=200&r=pg&d=mm";
+    }
+
     this.logout = function() {
         self.auth.$unauth();
         self.authData = null;
@@ -102,4 +147,7 @@ angular.module('firebaseHelper', [])
             });
 
     }
-});
+})
+
+
+;
